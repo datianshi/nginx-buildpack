121803db128d6cef2730d5c42f207757d3b2ad6c
Shaozhen Ding <dsz0111@gmail.com>
2014-07-09 10:39:53 -0400

    test apache

---
+++ bin/test-boot.sh
@@ -0,0 +1,7 @@
+export APP_ROOT=/home/vcap/app
+
+echo "PORT is: $PORT"
+mv $APP_ROOT/public/httpd.conf $APP_ROOT/public/httpd.conf.org
+erb $APP_ROOT/public/httpd.conf.org > $APP_ROOT/public/httpd.conf
+(tail $APP_ROOT/httpd/logs/*log &)
+exec $APP_ROOT/httpd/bin/apachectl -f $APP_ROOT/httpd.conf
