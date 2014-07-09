b81d9030ec875a22cb6e8b0fa0d11f77bec64569
Shaozhen Ding <dsz0111@gmail.com>
2014-07-09 10:49:30 -0400

    Update test-boot.sh

--- bin/test-boot.sh
+++ bin/test-boot.sh
@@ -4,4 +4,4 @@ echo "PORT is: $PORT"
 mv $APP_ROOT/public/httpd.conf $APP_ROOT/public/httpd.conf.org
 erb $APP_ROOT/public/httpd.conf.org > $APP_ROOT/public/httpd.conf
 (tail $APP_ROOT/httpd/logs/*log &)
-exec $APP_ROOT/httpd/bin/apachectl -f $APP_ROOT/public/httpd.conf
+$APP_ROOT/httpd/bin/apachectl -f $APP_ROOT/public/httpd.conf
